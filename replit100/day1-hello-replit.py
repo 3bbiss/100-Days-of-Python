3127121ea101bf20617d3547d0d983d5040f3d82
3bbiss <51114369+3bbiss@users.noreply.github.com>
2024-02-25 20:29:54 -0500

    Initial commit. Day 1.

---
+++ replit100/day1-hello-replit.py
@@ -0,0 +1,7 @@
+print("Ali Abbiss")
+print("February 25th, 2024")
+print("""I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit (might use pycharm too), an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+print("I am feeling 🧐 about this challenge.")
+print("You can follow my progress at replit.com/@3bbiss")
